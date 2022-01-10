b5a9e4029ab1b9b0d91ea7dd5de748601859da39
DavideBernardini <davidebernardini09@gmail.com>
2022-01-11 00:26:53 +0100

    install+basic-configuration

---
+++ webpack.config.js
@@ -0,0 +1,11 @@
+// jshint esversion: 6
+
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+};
