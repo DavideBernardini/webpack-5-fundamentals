71fb4885e98eb811de824eb7566b7872001b2a74
DavideBernardini <davidebernardini09@gmail.com>
2022-01-11 01:30:13 +0100

    add dev server + hmr

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,21 @@
 // jshint esversion: 6
 
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: './src/index.js',
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-  },
+    mode: 'development',
+    entry: './src/index.js',
+    devServer: {
+        static: './dist',
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Development',
+        }),
+    ],
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
 };
